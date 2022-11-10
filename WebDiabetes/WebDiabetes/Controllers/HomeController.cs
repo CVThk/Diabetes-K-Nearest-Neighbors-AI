9632faacae88649b55c083f1e13540b26b6dabd5
C.V.T <thinh.chauvan2405@gmail.com>
2022-11-10 21:12:11 +0700

    complete graph test

--- WebDiabetes/WebDiabetes/Controllers/HomeController.cs
+++ WebDiabetes/WebDiabetes/Controllers/HomeController.cs
@@ -11,13 +11,14 @@ namespace WebDiabetes.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        static List<KNN_item> _dataDiabetes = DiabetesData.Instance.GetDiabetes();
+        static int K = 9;
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult Dataset()
         {
+            List<ClassDiabetes> _dataDiabetes = ConnectionDiabetesDatabase.Instance.GetDiabetes();
             string str = "";
             foreach (var k in _dataDiabetes)
             {
@@ -32,10 +33,11 @@ namespace WebDiabetes.Controllers
             ViewBag.CountPos = "[" + _dataDiabetes.Count(x => x.Val == 0).ToString() + "]";
             ViewBag.CountNe = "[" + _dataDiabetes.Count(x => x.Val == 1).ToString() + "]";
             double Precision, Recall;
-            KNN _knn = new KNN();
+            KNeighbors _knn = new KNeighbors(K);
             _knn.Fit(_dataDiabetes);
-            //_knn.K();
-            _knn.Accuracy(out Precision, out Recall, 11);
+            _knn.Accuracy(out Precision, out Recall);
+            ViewBag.Precision = "[" + (Precision * 100).ToString().Replace(",", ".") + "," + (100 - Precision * 100).ToString().Replace(",", ".") + "]";
+            ViewBag.Recall = "[" + (Recall * 100).ToString().Replace(",", ".") + "," + (100 - Recall * 100).ToString().Replace(",", ".") + "]";
 
             return View();
         }
@@ -47,26 +49,50 @@ namespace WebDiabetes.Controllers
 
         public ActionResult Result(FormCollection fc)
         {
-            double Pregnancies, Glucose, BloodPressure, Age, SkinThickness, DiabetesPedigreeFunction, Insulin, Weight, Height;
+            List<ClassDiabetes> _dataDiabetes = ConnectionDiabetesDatabase.Instance.GetDiabetes();
+            double Pregnancies, Glucose, BloodPressure, Age, SkinThickness, DiabetesPedigreeFunction, Insulin, BMI;
             if (!Utilities.Instance.ConvertDouble(fc["Pregnancies"].ToString(), out Pregnancies) || !Utilities.Instance.ConvertDouble(fc["Glucose"].ToString(), out Glucose)
                 || !Utilities.Instance.ConvertDouble(fc["BloodPressure"].ToString(), out BloodPressure) || !Utilities.Instance.ConvertDouble(fc["Age"].ToString(), out Age)
                 || !Utilities.Instance.ConvertDouble(fc["SkinThickness"].ToString(), out SkinThickness) || !Utilities.Instance.ConvertDouble(fc["DiabetesPedigreeFunction"].ToString(), out DiabetesPedigreeFunction)
-                || !Utilities.Instance.ConvertDouble(fc["Insulin"].ToString(), out Insulin) || !Utilities.Instance.ConvertDouble(fc["Weight"].ToString(), out Weight)
-                || !Utilities.Instance.ConvertDouble(fc["Height"].ToString(), out Height))
+                || !Utilities.Instance.ConvertDouble(fc["Insulin"].ToString(), out Insulin) || !Utilities.Instance.ConvertDouble(fc["BMI"].ToString(), out BMI))
             {
                 Session["Err"] = "true";
                 return RedirectToAction("Test");
-            }    
+            }
 
 
-            KNN _knn = new KNN();
+            KNeighbors _knn = new KNeighbors(K);
             _knn.Fit(_dataDiabetes);
+            ClassDiabetes dataTest = new ClassDiabetes();
+            dataTest.Attributes.Add(Pregnancies);
+            dataTest.Attributes.Add(Glucose);
+            dataTest.Attributes.Add(BloodPressure);
+            dataTest.Attributes.Add(SkinThickness);
+            dataTest.Attributes.Add(Insulin);
+            dataTest.Attributes.Add(BMI);
+            dataTest.Attributes.Add(DiabetesPedigreeFunction);
+            dataTest.Attributes.Add(Age);
+            double val = _knn.Predict(dataTest);
 
-            string avgResult, maxResult;
-            _knn.GraphString(out avgResult, out maxResult);
+            dataTest.Val = (int)val;
 
-            ViewBag.avgResult = avgResult;
-            ViewBag.maxResult = maxResult;
+            string avgSick, maxSick;
+            _knn.GraphString(out avgSick, out maxSick);
+            string testString = "[";
+            for (int i = 0; i < dataTest.Attributes.Count; i++)
+            {
+                if (i > 0)
+                    testString += ',';
+                testString += dataTest.Attributes[i].ToString().Replace(',', '.');
+            }
+            testString += "]";
+
+            ViewBag.dataTest = dataTest;
+
+            ViewBag.Test = testString;
+
+            ViewBag.avgSick = avgSick;
+            ViewBag.maxSick = maxSick;
 
             return View();
         }
